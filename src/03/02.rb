ecb47eeb0805d83bd351a95c412ef9bbfeebc315
Ben Papillon <ben.papillon@relaypayments.com>
2021-12-03 21:34:13 -0500

    style

--- src/03/02.rb
+++ src/03/02.rb
@@ -1,19 +1,19 @@
 inputs = File.read('inputs/03/01.txt').split("\n")
 
-def binaryToDecimal(binaryString)
-  binaryString.reverse.chars.map.with_index do |digit, index|
+def binary_to_decimal(binary_string)
+  binary_string.reverse.chars.map.with_index do |digit, index|
     digit.to_i * 2**index
   end.sum
 end
 
-def getRating(numbers, comparison=:max_by, defaultBit="1", idx=0)
-  positionBits = numbers.map { |x| x[idx] }
-  freq = positionBits.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
-  bit = freq.values.uniq.length == 2 ? positionBits.send(comparison){ |v| freq[v] } : defaultBit
+def get_rating(numbers, comparison=:max_by, default_bit="1", idx=0)
+  position_bits = numbers.map { |x| x[idx] }
+  freq = position_bits.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  bit = freq.values.uniq.length == 2 ? position_bits.send(comparison){ |v| freq[v] } : default_bit
   matches = numbers.select{ |number| number[idx] == bit }
-  return getRating(matches, comparison, defaultBit, idx + 1) if matches.length > 0
+  return get_rating(matches, comparison, default_bit, idx + 1) if matches.any?
 
-  binaryToDecimal(numbers[-1])
+  binary_to_decimal(numbers[-1])
 end
 
-puts getRating(inputs) * getRating(inputs, :min_by, "0")
+puts get_rating(inputs) * get_rating(inputs, :min_by, "0")
