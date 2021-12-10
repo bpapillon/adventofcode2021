fd7f265b7b263ecc1c79f33abaf854dd45c4bf1e
Ben Papillon <benpapillon@gmail.com>
2021-12-09 21:31:44 -0500

    maybe more readable

--- src/09/02.rb
+++ src/09/02.rb
@@ -1,31 +1,20 @@
 INPUT_FILE = 'inputs/09/01.txt'
 # INPUT_FILE = 'inputs/09/example.txt'
 
-def basin_adjacents(rows, row_idx, char_idx)
-  adjacent = adjacent_positions(rows, row_idx, char_idx)
-  expansion = adjacent.select do |expansion_row, expansion_char|
+def basin_neighbors(rows, row_idx, char_idx)
+  expansion = neighbor_positions(rows, row_idx, char_idx).select do |expansion_row, expansion_char|
     expansion_val = rows[expansion_row][expansion_char]
     expansion_val != "9" && expansion_val > rows[row_idx][char_idx]
   end
   expansion + expansion.reduce([]) do |accum, (expansion_row, expansion_char)|
-    accum + basin_adjacents(rows, expansion_row, expansion_char)
+    accum + basin_neighbors(rows, expansion_row, expansion_char)
   end
 end
 
-def adjacent_positions(rows, row_idx, char_idx)
-  adjacent = []
-  adjacent << [row_idx - 1, char_idx] if row_idx > 0
-  adjacent << [row_idx + 1, char_idx] if row_idx < rows.length - 1
-  adjacent << [row_idx, char_idx - 1] if char_idx > 0
-  adjacent << [row_idx, char_idx + 1] if char_idx < rows[row_idx].length - 1
-  adjacent
-end
-
 def get_basin(rows, row_idx, char_idx)
-  adjacent = adjacent_positions(rows, row_idx, char_idx)
-  return unless adjacent.map { |x, y| rows[x][y] }.sort[0] > rows[row_idx][char_idx]
+  return unless is_low_point?(rows, row_idx, char_idx)
 
-  [[row_idx, char_idx]] + basin_adjacents(rows, row_idx, char_idx)
+  [[row_idx, char_idx]] + basin_neighbors(rows, row_idx, char_idx)
 end
 
 def get_basins(rows)
@@ -39,6 +28,25 @@ def get_basins(rows)
     end
   end
   basins
+end
+
+def is_low_point?(rows, row_idx, char_idx)
+  neighbor_values(rows, row_idx, char_idx).sort[0] > rows[row_idx][char_idx]
+end
+
+def neighbor_positions(rows, row_idx, char_idx)
+  neighbor = []
+  neighbor << [row_idx - 1, char_idx] if row_idx > 0
+  neighbor << [row_idx + 1, char_idx] if row_idx < rows.length - 1
+  neighbor << [row_idx, char_idx - 1] if char_idx > 0
+  neighbor << [row_idx, char_idx + 1] if char_idx < rows[row_idx].length - 1
+  neighbor
+end
+
+def neighbor_values(rows, row_idx, char_idx)
+  neighbor_positions(rows, row_idx, char_idx).map do |neighbor_row, neighbor_char|
+    rows[neighbor_row][neighbor_char]
+  end
 end
 
 rows = File.read(INPUT_FILE).split("\n").map(&:chars)
